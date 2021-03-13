fae29a6b7b191bc82c54dc162f2e4438453e8626
eddy duroseau <eddyduroseau@gmail.com>
2021-03-13 14:00:07 -0500

    netflixproject

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Movies from './components/Movies/Movies.component.jsx'
-import SingleMovie from './components/SingleMovie/SingleMovie.component'
+import Form from "./components/SingleMovie/SingleMovie.component"
+import SingleMovie from "./components/SingleMovie/SingleMovie.component";
+import Movies from "./components/Movies/Movies.component";
 
 import "./App.css";
 
@@ -9,13 +10,27 @@ function App() {
     <Router>
       <div>
         <Switch>
-       
+          <Route exact path="/form" component={Form} />
           <Route exact path="/movies" component={Movies} />
-          <Route exact path="/singleMovie/:id" component={SingleMovie} />
+          <Route exact path="/SingleMovie/:id" component={SingleMovie} />
+
         </Switch>
       </div>
     </Router>
   );
 }
+
+// function movies() {
+//   return Movies.map(movies(item)) {
+//     return (
+//       <div key={item.objectID}>
+//       <span>
+//       <a href={item.url}>{item.title}</a>
+//       </span>
+     
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Movies/Movies.component.jsx
+++ src/components/Movies/Movies.component.jsx
@@ -6,32 +6,35 @@ class Movies extends Component {
     constructor(){
         super() 
         this.state = { 
-            movies: [],
+            movies: [
+                
+            ],
         }
     }
 
 
     componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/list/1?api_key=67095d224daa8dc607d145fc46fb7c47`)
+        fetch(`https://api.themoviedb.org/3/movie/550?api_key=b33d2c40889610e162182aa0c6178840`)
             .then(data => data.json())
-            .then(response => response)
-            .then(data => this.setState({movies: data.items}))
+            .then(response => console.log(response))
+            // .then(data => this.setState({movies: data.items}))
     }
 
 
 
     render(){
-console.log(this.state.movies)
+       console.log(this.state.movies)
         return(
             <div>
                 <h1>Movies</h1>
                 {this.state.movies.map(movie => {
-                    return(
+                    
                         <div>
                             <p>{movie.title}</p>
                             <Link to={{pathname:`singleMovie/${movie.id}`, state: movie}}><button>View Movie</button></Link>
+                            
                         </div>
-                    )
+                    
                 })}
             </div>
         )
